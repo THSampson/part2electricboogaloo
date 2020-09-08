b9b9f2449f4b502c9d120a1487dc75d61e3df300
Kevin Curnow <kevinscurnow@gmail.com>
2020-09-08 16:06:35 -0400

    added logic to grab location

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [longitude, setLongitude] = useState();
+  const [latitude, setLatitude] = useState();
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } 
+  }
+  const showPosition = (position) => {
+    setLatitude(position.coords.latitude);
+    setLongitude(position.coords.longitude);
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {getLocation()}
+      <p>{latitude}</p>
+      <p>{longitude}</p>
     </div>
   );
 }
