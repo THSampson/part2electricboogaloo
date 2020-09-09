3fe765b2088b0d9a34ad5598184d3b835e59b360
Gurpreet Singh <er.gurpreet.2017@gmail.com>
2020-09-09 12:18:23 -0400

    working fetch

--- src/components/nasaImg/NasaImg.js
+++ src/components/nasaImg/NasaImg.js
@@ -1,55 +1,22 @@
 import React, { useState } from 'react';
-// import {Button} from 'reactstrap';
-import "./Nasa.css";
 
-const baseURL='https://api.nasa.gov/planetary/earth/imagery';
-const key ='1XFhPkw82Rx977TcYcUcCFHWqhQZFpwdvckZYbIq';
+// const baseURL='https://api.nasa.gov/planetary/earth/imagery';
+// const key ='1XFhPkw82Rx977TcYcUcCFHWqhQZFpwdvckZYbIq';
 
-
-const NasaImg = () => {
-    const [lon, setLon]= useState('');
-    const [lat, setLat]= useState('');
-    const [date, setDate]= useState();
-
-
-    const fetchResults = () => {
-        let url= `${baseURL}?lon=${lon}&lat=${lat}&date=${date}&api_key=${key}`;
-        
-        fetch(url, {
-            method: 'GET',
-            headers: new Headers({
-                'Content-Type': 'application/json'
-            })
-        })
+const NasaImg = (props) => {
+    const [image, setImage]= useState();
+        fetch (`https://api.nasa.gov/planetary/earth/assets?lon=${props.longitude}&lat=${props.latitude}&date=2019-01-09&&dim=0.20&api_key=1XFhPkw82Rx977TcYcUcCFHWqhQZFpwdvckZYbIq`)
         .then(res => res.json())
-        .then(results => console.log(results) )
+        .then(json => {console.log(json); setImage(json)})
         .catch(err => console.log(err))
-        
-    };
-    const handleSubmit= (event) => {
-        event.preventDefault();
-        fetchResults();
-    }
 
-    return (
-        <div className="main" >
-            <div className="mainDiv">
-            <form>
-                <span>Enter a longitude: </span>
-                <input type="float" name="lon" onChange={(e) => setLon(e.target.value)} /> 
-                <br />
-                <span>Enter a latitude: </span>
-                <input type="float" name="lat"  onChange={(e) => setLat(e.target.value)} />
-                <br />
-                <span>Enter Date: </span>
-                <input type="date" name="date" pattern="[0-9]{8}" onChange={(e) => setDate(e.target.value)} />
-                <br />
-                <button className="submit" onClick={e => handleSubmit(e)}>Submit search</button>
-            </form>
+    if (!image) return <div />
 
+      return (
+          <div>
+             <img id="nasaImage"src={image.url} alt="img"/>
             </div>
-        </div>
-    )
-}
+    );
 
+}
 export default NasaImg;
